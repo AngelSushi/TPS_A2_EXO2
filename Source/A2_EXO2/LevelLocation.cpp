5774e2908d1ecc3200361e80c1b7e6adcab562f7
AngelSushi <angelsushigame@gmail.com>
2022-01-19 15:33:57 +0100

    fix crash collision

--- Source/A2_EXO2/LevelLocation.cpp
+++ Source/A2_EXO2/LevelLocation.cpp
@@ -22,7 +22,7 @@ void ALevelLocation::BeginPlay() {
 void ALevelLocation::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
 	UCustomGameInstance* gameInstance = Cast<UCustomGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
 
-	if (!gameInstance)
+	if (!gameInstance || !save)
 		return;
 
 	gameInstance->currentLocationName = locationName;
